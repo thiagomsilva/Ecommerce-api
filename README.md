5cc6ceefacfdac74be295f84971c1cb067d6921c
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2022-09-11 14:20:25 -0300

    add config mailcatcher

--- README.md
+++ README.md
@@ -4,14 +4,33 @@
 
 Repositório da API de e-commerce de jogos online do bootcamp OneBitCode
 
-## Tecnologias
+## Tecnologias instaladas
 
-Ruby 2.7.1
-Rails 6.0.3.3
-Postgresql
+- Ruby 2.7.1
+- Rails 6.0.3.3
+- Postgresql
+- Mailcatcher
+
+## Criando do banco de dados
+
+```
+rails db:create
+```
 
 ## Iniciando o projeto Back end
 
 ```
 rails s
+```
+
+## Endereço local
+
+```
+http://localhost:3000
+```
+
+## Endereço mailcatcher local
+
+```
+http://localhost:1080
 ```
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,15 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors =
+    config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'localhost',
+    port: 1025
+  }
 
   config.action_mailer.perform_caching = false
 
@@ -41,7 +49,6 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
-
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
