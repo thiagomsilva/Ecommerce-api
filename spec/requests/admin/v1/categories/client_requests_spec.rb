7775ce03c8756a211b053cf8ce366984b5f3a845
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2023-01-31 19:08:30 -0300

    config restrict access

--- spec/requests/admin/v1/categories/client_requests_spec.rb
+++ spec/requests/admin/v1/categories/client_requests_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "Admin V1 Categories as :client", type: :request do
-  let(:user) { create(:user) }
-
+  let(:user) { create(:user, profile: :client) }
+  
   context "GET /categories" do
     let(:url) { "/admin/v1/categories" }
     let!(:categories) { create_list(:category, 5) }
