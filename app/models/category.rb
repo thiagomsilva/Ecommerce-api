568e621124be40a077ed294e62fa0d2851f7cd43
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2023-02-06 21:45:59 -0300

    Add concern paginate in model category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,8 @@
 class Category < ApplicationRecord
-  include NameSearchable # incluindo o concern
+  # incluindo os concerns
+  include NameSearchable
+  include Paginatable
+
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 
